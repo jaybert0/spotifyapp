3de062ea2ac4cd8a14ca9ad6a382b461e3839056
Jennifer Tchai <jentchai@gmail.com>
2022-04-09 16:10:52 -0400

    importing icons

---
+++ components/sidebar.tsx
@@ -0,0 +1,8 @@
+import { Box, List, ListItem, ListIcon, Divider, Center, LinkBox, LinkOverlay } from '@chakra-ui/layout';
+import { MdHome, MdSearch, MdLibraryMusic, MdPlaylistAdd, MdFavorite } from 'react-icons/md';
+
+export default function Sidebar() {
+    return (
+        <div>hello from sidebar</div>
+    )
+}
