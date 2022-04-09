6a56938fd8c184caf356969dceb567bfe17f218f
Jennifer Tchai <jentchai@gmail.com>
2022-04-09 16:39:51 -0400

    added first three sidebar links

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,6 +1,25 @@
 import { Box, List, ListItem, ListIcon, Divider, Center, LinkBox, LinkOverlay } from '@chakra-ui/layout';
 import { MdHome, MdSearch, MdLibraryMusic, MdPlaylistAdd, MdFavorite } from 'react-icons/md';
 import NextImage from 'next/image';
+import NextLink from 'next/link';
+
+const navMenu = [
+    {
+        name: 'Home',
+        icon: MdHome,
+        route: '/'
+    },
+    {
+        name: 'Search',
+        icon: MdSearch,
+        route: '/search'
+    },
+    {
+        name: 'Your Library',
+        icon: MdLibraryMusic,
+        route: '/library'
+    }
+]
 
 export default function Sidebar() {
     return (
@@ -13,7 +32,23 @@ export default function Sidebar() {
         >
             <Box paddingY="20px">
                 <Box width="120px" marginBottom="20px" paddingX="20px">
-                    <NextImage src="/logo.png" height={90} width={250} />
+                    <NextImage src="/logo.png" height={100} width={300} />
+                </Box>
+                <Box marginBottom="20px">
+                    <List spacing={2}>
+                        {navMenu.map(menu => (
+                            <ListItem paddingX='20px' fontSize='16px' key={menu.name}>
+                                <LinkBox>
+                                    <NextLink href={menu.route} passHref>
+                                        <LinkOverlay>
+                                            <ListIcon as={menu.icon} color='white' marginRight='20px' />
+                                            {menu.name}
+                                        </LinkOverlay>
+                                    </NextLink>
+                                </LinkBox>
+                            </ListItem>
+                        ))}
+                    </List>
                 </Box>
             </Box>
         </Box>
