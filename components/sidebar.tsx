8ecc87caea1f8e85781593757be41c179bc1a3b8
Jennifer Tchai <jentchai@gmail.com>
2022-04-09 16:19:32 -0400

    starting to build sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,8 +1,19 @@
 import { Box, List, ListItem, ListIcon, Divider, Center, LinkBox, LinkOverlay } from '@chakra-ui/layout';
 import { MdHome, MdSearch, MdLibraryMusic, MdPlaylistAdd, MdFavorite } from 'react-icons/md';
+import NextImage from 'next/image';
 
 export default function Sidebar() {
     return (
-        <div>hello from sidebar</div>
+        <Box 
+            width='100%' 
+            height='calc(100vh - 100px)' 
+            bg='black' 
+            paddingX='5px' 
+            color='gray'
+        >
+            <Box paddingY="20px">
+                <Box width="120px" marginBottom="20px" paddingX="20px"></Box>
+            </Box>
+        </Box>
     )
 }
