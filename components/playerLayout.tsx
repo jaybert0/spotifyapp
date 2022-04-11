3baa61e4271fb6d52544bc344527f965dbcb7065
J Lee <jay.j.lee.1@gmail.com>
2022-04-11 14:55:17 -0400

    prisma

--- components/playerLayout.tsx
+++ components/playerLayout.tsx
@@ -1,18 +1,19 @@
-import { Box } from '@chakra-ui/layout';
-import Sidebar from './sidebar';
+import { Box } from "@chakra-ui/layout";
+import Sidebar from "./sidebar";
 
+// eslint-disable-next-line react/function-component-definition
 export default function PlayerLayout({ children }) {
-    return(
-        <Box width="100vw" height="100vh">
-            <Box position="absolute" top="0" width="250px" left="0">
-                <Sidebar />
-            </Box>
-            <Box marginLeft="250px" marginBottom="100px">
-                {children}
-            </Box>
-            <Box position="absolute" left="0" bottom="0">
-                player
-            </Box>
-        </Box>
-    )
+  return (
+    <Box width="100vw" height="100vh">
+      <Box position="absolute" top="0" width="250px" left="0">
+        <Sidebar />
+      </Box>
+      <Box marginLeft="250px" marginBottom="100px">
+        {children}
+      </Box>
+      <Box position="absolute" left="0" bottom="0">
+        player
+      </Box>
+    </Box>
+  );
 }
