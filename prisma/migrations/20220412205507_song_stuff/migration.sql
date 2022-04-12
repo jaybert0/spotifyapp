61b0c62ca37e932a7f69b85afb655defd3da82e1
J Lee <jay.j.lee.1@gmail.com>
2022-04-12 16:56:39 -0400

    done with migrations

---
+++ prisma/migrations/20220412205507_song_stuff/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - Added the required column `duration` to the `Song` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `url` to the `Song` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropIndex
+DROP INDEX "Playlist_name_key";
+
+-- DropIndex
+DROP INDEX "Song_name_key";
+
+-- AlterTable
+ALTER TABLE "Song" ADD COLUMN     "duration" INTEGER NOT NULL,
+ADD COLUMN     "url" TEXT NOT NULL;
